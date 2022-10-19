80a85380019ede77a5df577f42720e054e9b341f
nlankin <nikita.lankin@cengage.com>
2022-10-19 13:46:13 +0300

    add UserService.js and UserComponent.js

---
+++ src/Components/UserComponent.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react'
+import UserService from "../Services/UserService";
+
+function UserComponent() {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    const getUsers = () => {
+
+        UserService.getUsers().then((response) => {
+            setUsers(response.data)
+            console.log(response.data);
+        });
+    };
+
+    return (
+        <div className = "container">
+
+            <h1 className = "text-center"> User List</h1>
+
+            <table className = "table table-striped">
+                <thead>
+                <tr>
+                    <th> User Id</th>
+                    <th> User Login</th>
+                    <th> User Credentials</th>
+                    <th> User Email</th>
+                    <th> User Gender</th>
+                    <th> User birthday</th>
+                </tr>
+
+                </thead>
+                <tbody>
+                {
+                    users.map(
+                        user =>
+                            <tr key = {user.userId}>
+                                <td> {user.userId}</td>
+                                <td> {user.login }</td>
+                                <td> {user.credentials }</td>
+                                <td> {user.email }</td>
+                                <td> {user.gender }</td>
+                                <td> {user.birthday ? null : 'not specified'}</td>
+                            </tr>
+
+                    )
+                }
+
+                </tbody>
+
+
+            </table>
+
+        </div>
+    )
+}
+
+export default UserComponent
