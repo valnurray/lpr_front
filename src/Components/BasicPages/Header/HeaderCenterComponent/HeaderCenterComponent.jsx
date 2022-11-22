3350a2ffbc904228285a967da76c5519f17ae3d9
nlankin <nikita.lankin@cengage.com>
2022-11-22 15:12:56 +0200

    Search css

--- src/Components/BasicPages/Header/HeaderCenterComponent/HeaderCenterComponent.jsx
+++ src/Components/BasicPages/Header/HeaderCenterComponent/HeaderCenterComponent.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import style from "./HeaderCenterComponent.module.css"
 import 'font-awesome/css/font-awesome.min.css';
+import search from '../../../../common/images/Search/search.png'
 
 const HeaderCenterComponent = () => {
     return <div className={style.headerCenter}>
-        {/*<div className={style.box}>*/}
-        {/*    <form name="search">*/}
-        {/*        /!*<label htmlFor="se">Choose a username: </label>*!/*/}
-        {/*        <input id="search" type="text" className={style.input} name="txt" onMouseOut="this.value = ''; this.blur();" />*/}
-        {/*    </form>*/}
-        {/*    <i className="fas fa-search"/>*/}
-        {/*</div>*/}
-        <h2>title</h2>
+        <div className={style.box}>
+            <form name="search">
+                <input id="search" type="text" className={style.input} name="txt" onMouseOut="this.value = ''; this.blur();" />
+            </form>
+            {/*<i className="fas fa-search"/>*/}
+            <i>
+                <img src={search} alt={search}/>
+            </i>
+        </div>
     </div>
 
 
