715de8df015ea890771d1a7cbf330150e874d00a
nlankin <nikita.lankin@cengage.com>
2022-12-03 00:03:31 +0200

    add some features

--- src/Components/BuisnessComponents/Category/ViewCategoryComponent.js
+++ src/Components/BuisnessComponents/Category/ViewCategoryComponent.js
@@ -3,6 +3,7 @@ import {useNavigate} from 'react-router-dom';
 import {useParams} from "react-router-dom";
 import styles from './CategoryComponent.module.css'
 import CategoryService from "../../../Services/CategoryService";
+import img from "../../../common/images/Products/LifeBeyond/life beyond_2.jpg"
 
 function ViewCategoryComponent() {
     const [category, setCategory] = useState({
@@ -21,7 +22,6 @@ function ViewCategoryComponent() {
     const navigate = useNavigate();
 
     const {id} = useParams();
-
 
     useEffect(() => {
         getCategory()
@@ -46,24 +46,22 @@ function ViewCategoryComponent() {
         <div className={styles.categoryWraper}>
                 <h3 className="text-center">{category.title}</h3>
                 <div className="card-body">
-                        <h2 className={styles.naming}> About:  {category.description}</h2>
-                    <div className="row">
+                        <h2 className={styles.naming}>{category.description}</h2>
+                    <div className={styles.container2}>
                         {
                             category.products.map(( item =>
                             <div className={styles.productdiv} key={item.productId} onClick={() => viewProduct(item.productId)}>
-                                <div>{item.title}</div>
-                                <div>{item.price}</div>
-                                <div>{item.info}</div>
-                                <div>{item.imgpath}</div>
-                                <div>{item.visibility}</div>
+                                <div className={styles.rows}>
+                                    <div>{item.title}</div>
+                                    <div>{item.price}</div>
+                                    <div>{item.info}</div>
+                                    {/*<div><img src={img}/></div>*/}
+                                    {/*<div>{item.visibility}</div>*/}
+                                </div>
                             </div>
                             ))
                         }
-                    {/*    <label> Category Title: {category.}</label>*/}
                     </div>
-                    {/*<div className="row">*/}
-                    {/*    <label> Category Description: {category.credentials}</label>*/}
-                    {/*</div>*/}
                 </div>
 
             <button className={styles.homebutton} onClick={cancel}>BACK
