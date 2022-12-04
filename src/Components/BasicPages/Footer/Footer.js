9db3fdcfaf199ad807ad673480d06fa187a59d2b
nlankin <nikita.lankin@cengage.com>
2022-12-05 00:04:18 +0200

    add some features

--- src/Components/BasicPages/Footer/Footer.js
+++ src/Components/BasicPages/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
-import classes from "./Footer.module.css";
+import styles from "./Footer.module.css"
+import footerlogo from "../../../common/icons/footerlogo.png"
 
 const Footer = () => {
-    return <footer className={classes.footer}>
-        <div className = "container">
-            <h1 className = "text-center"> FOOTER </h1>
+    return <footer className={styles.footer}>
+        <div className ={styles.footerholder}>
+            {/*<h1 className = "text-center"> FOOTER </h1>*/}
+            <p className={styles.copy}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. 2022(c)
+            </p>
+            <div className={styles.footerlogo}>
+                <img className={styles.footerlogo} src={footerlogo} alt="footerlogo"/>
+            </div>
         </div>
     </footer>
 }
