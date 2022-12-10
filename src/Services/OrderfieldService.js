68c223955ba6bbb6c328a4ba10f91737875216cd
nlankin <nikita.lankin@cengage.com>
2022-12-10 17:43:43 +0200

    add some features

---
+++ src/Services/OrderfieldService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const ORDERFIELD_API_BASE_URL = 'http://localhost:8080/api/orderfield';
+
+class OrderfieldService{
+
+    getOrderfield(){
+        return axios.get(ORDERFIELD_API_BASE_URL);
+    }
+
+    getOrderfield(orderId){
+        return axios.get(ORDERFIELD_API_BASE_URL + '/' + orderId)
+    }
+}
+
+export default new OrderfieldService();
